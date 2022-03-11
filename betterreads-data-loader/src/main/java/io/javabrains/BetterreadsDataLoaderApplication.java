ed7ca9de6aaaedc39d4ffe1b374882099c9bc9f4
Ranjitha Nayak <ranjithanayak48@gmail.com>
2022-03-11 22:11:48 +0530

    Using Repository Pattern with Spring Data

--- betterreads-data-loader/src/main/java/io/javabrains/BetterreadsDataLoaderApplication.java
+++ betterreads-data-loader/src/main/java/io/javabrains/BetterreadsDataLoaderApplication.java
@@ -2,17 +2,25 @@ package io.javabrains;
 
 import java.nio.file.Path;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import io.javabrains.author.Author;
+import io.javabrains.author.AuthorRepository;
 import io.javabrains.connection.DataStaxAstraProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
+	
+	@Autowired
+	AuthorRepository authorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
@@ -23,5 +31,17 @@ public class BetterreadsDataLoaderApplication {
         Path bundle = astraProperties.getSecureConnectBundle().toPath();
         return builder -> builder.withCloudSecureConnectBundle(bundle);
     }
+	
+	@PostConstruct
+	public void start() {
+		//System.out.println("Application Started");
+		
+		Author author = new Author();
+		author.setId("id");
+		author.setName("name");
+		author.setPersonalName("personalName");
+		
+		authorRepository.save(author);
+	}
 
 }
