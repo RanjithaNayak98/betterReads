bfcca21bd08bd091e15fac510ac01153f6c020df
Ranjitha Nayak <ranjithanayak48@gmail.com>
2022-03-13 15:31:32 +0530

    Saving authors to Cassandra

--- betterreads-data-loader/src/main/java/io/javabrains/BetterreadsDataLoaderApplication.java
+++ betterreads-data-loader/src/main/java/io/javabrains/BetterreadsDataLoaderApplication.java
@@ -1,10 +1,17 @@
 package io.javabrains;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import javax.annotation.PostConstruct;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
@@ -18,30 +25,59 @@ import io.javabrains.connection.DataStaxAstraProperties;
 @SpringBootApplication
 @EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterreadsDataLoaderApplication {
-	
+
 	@Autowired
 	AuthorRepository authorRepository;
+
+	@Value("${datadump.location.author}")
+	private String authorDumpLocation;
+
+	@Value("${datadump.location.works}")
+	private String worksDumpLocation;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetterreadsDataLoaderApplication.class, args);
 	}
-	
+
 	@Bean
-    public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
-        Path bundle = astraProperties.getSecureConnectBundle().toPath();
-        return builder -> builder.withCloudSecureConnectBundle(bundle);
-    }
-	
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
+
+	private void initAuthors() {
+		Path path = Paths.get(authorDumpLocation);
+		try {
+			Stream<String> lines =  Files.lines(path);
+			lines.forEach(line ->{
+				String jsonString =  line.substring(line.indexOf("{"));	
+				try {
+					JSONObject jsonObject = new JSONObject(jsonString);
+					Author author = new Author();
+					author.setName(jsonObject.optString("name"));
+					author.setPersonalName(jsonObject.optString("personal_name"));
+					author.setId(jsonObject.optString("key").replace("/authors/", ""));
+					System.out.println("saving author "+author.getName());
+					authorRepository.save(author);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	private void initWorks() {
+
+	}
+
+
 	@PostConstruct
 	public void start() {
-		//System.out.println("Application Started");
-		
-		Author author = new Author();
-		author.setId("id");
-		author.setName("name");
-		author.setPersonalName("personalName");
-		
-		authorRepository.save(author);
+		initAuthors();
+		initWorks();
 	}
 
 }
